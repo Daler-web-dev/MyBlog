c51866b967d8b0094847e2caaf18dee7bfcfe19d
Daler <dsarifkulo021@gmail.com>
2022-11-29 18:55:32 +0500

    transition were added to text and some tags were included to dark mode

--- components/_child/Newsletter.jsx
+++ components/_child/Newsletter.jsx
@@ -4,7 +4,7 @@ const Newsletter = () => {
 
 
     return (
-        <section className="bg-gray-50 mt-20">
+        <section className="newsletter bg-gray-50 mt-20">
             <div className="container mx-auto md:px-20 py-16 text-center">
                 <h1 className="font-bold text-3xl " >Subscribe Newsletter</h1>
                 <div className="py-4" >
--- pages/index.jsx
+++ pages/index.jsx
@@ -15,9 +15,9 @@ const Home = () => {
 			(!("color-theme" in localStorage) &&
 				window.matchMedia("(prefers-color-scheme: dark)").matches)
 		) {
-			document.documentElement.classList.add("dark");
-		} else {
 			document.documentElement.classList.remove("dark");
+		} else {
+			document.documentElement.classList.add("dark");
 		}
 	}, []);
 
