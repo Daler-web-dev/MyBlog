9bf02eac372ba753b6e04269b187e24f55d09824
Daler <dsarifkulo021@gmail.com>
2022-12-05 01:48:45 +0500

    blog page was done

--- components/_child/Author.jsx
+++ components/_child/Author.jsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-const Author = ({name, img, designation}) => {
+const Author = ({name, img, designation, col="text-gray-800"}) => {
     return (
         <div className="author flex py-5" >
             <Image src={img || "/images/author/author1.jpg"} alt="author" width="60" height="60" className="rounded-full" />
             <div className="flex flex-col justify-center px-4">
-                <Link href={"/"} ><a className="text-md font-bold text-gray-800 hover:text-gray-600">{name || "No Name"}</a></Link>
+                <Link href={"#"} ><a className={`text-md font-bold ${col} hover:text-gray-600`}>{name || "No Name"}</a></Link>
                 <span className="text-sm text-gray-500" >{designation || "NO designation"}</span>
             </div>
         </div>
--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,45 +1,66 @@
-import React from 'react';
-import Author from '../components/_child/Author';
-import DarkMode from '../components/_child/DarkMode';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Author from "../components/_child/Author";
+import DarkMode from "../components/_child/DarkMode";
 import Format from "../Layout/Format";
 
 const blog = () => {
-    return (
-        <Format>
-            <DarkMode/>
-            <center className="w-[90%] md:w-[60%] mx-auto flex flex-col gap-6 mt-[40px]" >
-                <span className='motivation'>BE TRULY FLEXIBLE</span>
-                <h1 className='text-[24px] leading-[26px]' >
-                    All jobs and gigs that fit your schedule in one place.
-                </h1>
-                <div className='w-[100%] flex gap-2 flex-wrap justify-center' >
-                    {
-                        ['HTML', 'CSS', "JavaScript", "React Js", "Redux", 'Next Js', 'Node Js'].map((item, index) => (
-                            <span key={index} className="tag" >{item}</span>
-                        ))
-                    }
-                </div>
-            </center>
-            <section className='container-grid' >
-                    <div className='flex flex-col rounded-t-[30px] bg-black overflow-hidden' >
-                        <div className="bottom-info  bg-[#1E5C8F] p-5">
-                            <h3>
-                                Course how to kill
-                            </h3>
-                            <span>I am joking</span>
-
-                            <div className="flex justify-between items-center">
-                                <Author/>  <span className="tag h-[100%]" >Category</span>
-                            </div>
-                        </div>
-                    </div>
+	return (
+		<Format>
+			<DarkMode />
+			<center className="w-[90%] md:w-[60%] mx-auto flex flex-col gap-6 mt-[40px]">
+				<span className="motivation">BE TRULY FLEXIBLE</span>
+				<h1 className="text-[24px] leading-[26px] md:text-[55px] md:leading-[55px]">
+					All jobs and gigs that fit your schedule in one place.
+				</h1>
+				<div className="w-[100%] flex gap-2 flex-wrap justify-center">
+					{[
+						"HTML",
+						"CSS",
+						"JavaScript",
+						"React Js",
+						"Redux",
+						"Next Js",
+						"Node Js",
+					].map((item, index) => (
+						<span key={index} className="tag">
+							{item}
+						</span>
+					))}
+				</div>
+			</center>
+			<section className="container-grid">
+                {[1,2,3,4,5,6].map(item => <BlogItem/>)}
             </section>
-        </Format>
-    );
+		</Format>
+	);
 };
 
 const BlogItem = () => {
-    return (<></>)
-}
+	return (
+		<div className="flex flex-col rounded-t-[30px] bg-black overflow-hidden">
+            <Link href={"#"} >
+                <img
+                    src="/images/img3.png"
+                    alt=""
+                    className="sm:h-[200px] md:h-[400px] object-cover"
+                />
+            </Link>
+			<div className="bottom-info  bg-[#1E5C8F] p-5 text-white">
+                <Link href={"#"} >
+                    <div>
+                        <h3 className="text-[24px]  " >Course how to kill</h3>
+                        <span>I am joking</span>
+                    </div>
+                </Link>
+
+				<div className="flex justify-between items-center">
+					<Author col="text-white" /> <span className="tag h-[100%]">Category</span>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default blog;
